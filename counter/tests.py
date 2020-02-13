54a7eefe0c7f71b7d788b10448f6b11a102eccce
yunhwan-k <yunhwan.k@navercorp.com>
2020-02-13 13:21:49 +0900

    Test: Django 테스트 유틸리티 확인

--- counter/tests.py
+++ counter/tests.py
@@ -1,3 +1,27 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+
+from counter.models import Counter
+from counter import views 
 
 # Create your tests here.
+class CounterTest(TestCase):
+
+    def test_first(self):
+        l = len(Counter.objects.all())
+        print(l)
+        self.assertEqual(l, 0)
+
+    def test_status(self):
+        c = Client()
+        # follow any redirection
+        # log in response.redirect_chain
+        # c.post('/counter/up', {'hello': 1})
+        # -> 결과 값으로 form data : multipart/form-data
+        response = c.post('/counter/up', follow=True)
+        print("response=", response)
+        print("status_code=", response.status_code)
+        print("context=", response.context)
+        print("Content-Type=", response['Content-Type'])
+
+
+
